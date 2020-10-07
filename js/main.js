4770a514da557b4408d92986cdfbeb5905d238ad
Tatiana <tati@MacBook-Air-Tatiana.local>
2020-10-07 19:24:41 +0300

    mobile adaptive added v.1

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ window.addEventListener('scroll', function(){
     Paths[0].setAttribute('startOffset', window.scrollY * 0.8);
     Paths[1].setAttribute('startOffset', window.scrollY *0.4);
     Paths[2].setAttribute('startOffset', window.scrollY *0.6);
-    Paths[3].setAttribute('startOffset', window.scrollY *0.3);
-    Paths[4].setAttribute('startOffset', window.scrollY);
 })
+
+const Button = document.querySelector('.header__menu-btn');
+
+const menuMobile = document.querySelector('.menu__mobile');
+
+Button.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    menuMobile.classList.toggle('menu__mobile-show');
+}
